2151bfa229281deda80aa590342daa83e7f1e246
zhangjiluo <zhang.jiluo@vpclub.cn>
2018-07-06 15:19:17 +0800

    fix(index.js): Got a default config object that there is no vue config for this plugin.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const VConsolePlugin = require('vconsole-webpack-plugin')
 
 module.exports = (api, projectOptions) => {
-  const {vconsole} = projectOptions.pluginOptions
+  const {vconsole} = projectOptions.pluginOptions || {}
   api.configureWebpack(webpackConfig => {
     webpackConfig.plugins.push(new VConsolePlugin(Object.assign({
       enable: process.env.NODE_ENV === 'development'
